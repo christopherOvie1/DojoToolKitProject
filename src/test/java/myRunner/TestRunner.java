670954144be6b3983022e6a416b7566e6c4015cb
covie <covie@planittesting.com>
2020-12-18 11:44:18 +0000

    first commit

--- src/test/java/myRunner/TestRunner.java
+++ src/test/java/myRunner/TestRunner.java
@@ -10,7 +10,6 @@ import org.junit.runner.RunWith;
 features = {"classpath:Problematic.Feature"},
         glue = {"classpath:stepDefinition"},
         monochrome = true,  //display the console in a proper readable format
-        dryRun  = false ,  //check the mapping is proper between the feature file and step def file
         plugin = {"pretty","html:target/cucumber-html-report",  "json:target/cucumber.json"}
                )
 public class TestRunner {
